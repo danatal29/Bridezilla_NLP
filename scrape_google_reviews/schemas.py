47dbfdf19eac66f4d456260683f635daa34e957c
dana <tdana29@gmail.com>
2024-03-23 00:55:04 +0200

    create scraper

--- scrape_google_reviews/schemas.py
+++ scrape_google_reviews/schemas.py
@@ -1,5 +1,8 @@
 from enum import Enum
+
+
 class Review(Enum):
+    # this class holds the name of each kind of 'review' class
     TEXT:str = 'wiI7pd'
     STARS:str = 'kvMYJc'
     TIME:str = 'rsqaWe'
