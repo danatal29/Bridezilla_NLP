014a19de642b49582c6379c327e3aa4325116e79
dana <tdana29@gmail.com>
2024-04-07 21:11:42 +0300

    data & summarize notebook

--- scrape_google_reviews/schemas.py
+++ scrape_google_reviews/schemas.py
@@ -3,8 +3,9 @@ from enum import Enum
 
 class Review(Enum):
     # this class holds the name of each kind of 'review' class
-    TEXT:str = 'wiI7pd'
-    STARS:str = 'kvMYJc'
-    TIME:str = 'rsqaWe'
+    TEXT:str = 'wiI7pd' 
+    STARS:str = 'kvMYJc' 
+    TIME:str = 'xRkPPb'  # 'rsqaWe'
+    
 
 
